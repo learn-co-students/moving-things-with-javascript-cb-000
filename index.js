a1b7b21f632791542b38332efebfdb4f659ff542
Sean Altemose <sean@seanaltemose.com>
2019-04-26 19:07:32 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerRight(){
   var rightNumbers = dodger.style.right.replace('px', '');
   var right = parseInt(rightNumbers, 10)
 
-  if (right < 0) {
+  if (right > 0) {
     dodger.style.right = `${right - 1}px`
   }
 
